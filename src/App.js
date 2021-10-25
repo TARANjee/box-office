bb0ed197e938094074c9fbb076274ee666adc37a
tjsingh <taranjeets120@gmail.com>
2021-10-25 23:11:15 +0530

    Creating Routes with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import {Switch,Route} from 'react-router-dom';
+
+
 
 function App() {
   return (
-    <div className="App">
-      Hi
-    </div>
+    <Switch>
+      <Route exact path="/">This is Home Page</Route>
+      <Route exact path="/starred">This is Starred Page</Route>
+      <Route>This is 404 Page</Route>
+    </Switch>
   );
 }
 
