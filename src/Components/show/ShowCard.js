a818a934396832adf1ac73e0d12fe13833ad3490
tjsingh <taranjeets120@gmail.com>
2021-10-28 23:51:22 +0530

    Added styled-component to grid and cards

--- src/Components/show/ShowCard.js
+++ src/Components/show/ShowCard.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import {StyledShowCard} from './ShowCardStyled'
 
-const ShowCard = ({id,name,image,summary}) => {
+const ShowCard = ({ id, name, image, summary }) => {
 
     const summaryAsText = summary
-    ? `${summary.split(' ').slice(0,10).join(' ').replace(/<.+?>/g, '')}...`
-    : 'No Description';
+        ? `${summary.split(' ').slice(0, 10).join(' ').replace(/<.+?>/g, '')}...`
+        : 'No Description';
 
     return (
-        <div>
-            <div>
-            <img src={image}/>
+        <StyledShowCard>
+            <div className="img-wrapper">
+                <img src={image} />
             </div>
             <h1>{name}</h1>
             <p>{summaryAsText}</p>
-            <div><Link to={`/show/${id}`} >Read more</Link></div>
-            <button type='button' >starred me</button>
-        </div>
+            <div className='btns'>
+                <Link to={`/show/${id}`} >Read more</Link>
+                <button type='button' >starred me</button>
+            </div>
+        </StyledShowCard>
     )
 }
 
